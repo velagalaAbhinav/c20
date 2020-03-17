f7aeae23bd57cece9252b307fada37e4e916e4c9
velagalaAbhinav <62015508+velagalaAbhinav@users.noreply.github.com>
2020-03-17 16:39:49 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,37 +1,24 @@
-var fixedrect, movingrect;
-
+var fixedRect, movingRect;
 
 function setup() {
   createCanvas(1200,800);
-  fixedrect = createSprite(600, 200, 50, 80);
-  fixedrect.shapeColor = "green";
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
 
-  movingrect = createSprite(400,200,50,80);
-  movingrect.shapeColor = "green";
-  movingrect.velocityX = 5;
-  fixedrect.velocityX = -5;
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
 }
 
 function draw() {
-  background(0,0,0);
-  //movingrect.y = World.mouseY;
- // movingrect.x = World.mouseX;  
-if(movingrect.x - fixedrect.x < movingrect.width/2 + fixedrect.width/2
-  && fixedrect.x - movingrect.x < movingrect.width/2 + fixedrect.width/2
-  && movingrect.y - fixedrect.y < movingrect.height/2 + fixedrect.height/2
-  && fixedrect.y - movingrect.y < movingrect.height/2 + fixedrect.height/2){
-movingrect.shapeColor = "red";
-fixedrect.shapeColor = "red";
-}
-else {
-movingrect.shapeColor = "green";
-fixedrect.shapeColor = "green";
-}
-if(movingrect.x - fixedrect.x < movingrect.width/2 + fixedrect.width/2
-  && fixedrect.x - movingrect.x < movingrect.width/2 + fixedrect.width/2){
-  movingrect.velocityX = movingrect.velocityX * -1;
-  fixedrect.velocityX = fixedrect.velocityX * -1;
+  background(0,0,0);  
 
-  }
+  bounceoff(movingRect,fixedRect);
   drawSprites();
+  
 }
+
+
